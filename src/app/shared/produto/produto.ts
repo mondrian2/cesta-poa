4784109f28ac3060686f7d122992fd9c984d5747
Daniel Faustino da Silva <danielfs@grupodimed.com.br>
2020-06-26 17:42:10 -0300

    produto cl

---
+++ src/app/shared/produto/produto.ts
@@ -0,0 +1,10 @@
+export class Produto {
+    id:Number
+    imagem: String[]
+    nome: String
+    descricao: String
+    caracteristicas: String
+    precoDe: String
+    precoPor: String
+    desconto: String
+}
